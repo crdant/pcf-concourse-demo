643337939384dc04f3fd249edcfce96541b611fb
Chuck D'Antonio <c_dantonio@acm.org>
2017-06-19 10:29:37 -0400

    Change to trigger a build

--- src/main/java/com/pivotal/example/xd/controller/OrderController.java
+++ src/main/java/com/pivotal/example/xd/controller/OrderController.java
@@ -28,72 +28,73 @@ import com.pivotal.example.xd.RabbitClient;
  */
 @Controller
 public class OrderController {
-	
+
 	@Autowired
 	ServletContext context;
-	
+    int flag = 0 ;
+
 	private static Map<String,Queue<Order>> stateOrdersMap = new HashMap<String, Queue<Order>>();
 	private static RabbitClient client ;
 
 	boolean generatingData = false;
-	
+
 	static Logger logger = Logger.getLogger(OrderController.class);
 
 	OrderGenerator generator = new OrderGenerator();
 	Thread threadSender = new Thread (generator);
-	
+
     public OrderController(){
-    	
+
     	client = RabbitClient.getInstance();
-    	
+
     	for (int i=0; i<HeatMap.states.length; i++){
     		stateOrdersMap.put(HeatMap.states[i], new ArrayBlockingQueue<Order>(10));
     	}
-    	
+
     	if(client.getRabbitURI() != null){
     		threadSender.start();
         	client.startMessageListener();
         	client.startOrderProcessing();
     	}
-    	
-    	
+
+
     }
-	
+
 	private int getOrderSum(String state){
-		
+
 		int sum = 0;
 		Queue<Order> q  = stateOrdersMap.get(state);
 		Iterator<Order> it = q.iterator();
 		while (it.hasNext()){
 			sum += it.next().getAmount();
 		}
-		
+
 		return sum;
 	}
-    
 
-	
+
+
 	public static synchronized void registerOrder(Order order){
 		Queue<Order> orderQueue = stateOrdersMap.get(order.getState());
 		if (!orderQueue.offer(order)){
 			orderQueue.remove();
 			orderQueue.add(order);
-		}				
+		}
 	}
-    
+
 	@RequestMapping(value = "/")
 	public String home(Model model) throws Exception{
 		model.addAttribute("rabbitURI", client.getRabbitURI());
-		
+
 		ObjectMapper mapper = new ObjectMapper();
-		
-		
+
+
 		//add details about VCAP APPLICATION
 		if(System.getenv("VCAP_APPLICATION") != null){
 			Map vcapMap = mapper.readValue(System.getenv("VCAP_APPLICATION"), Map.class);
 			model.addAttribute("vcap_app", vcapMap);
 		}
-		
+
         return "WEB-INF/views/pcfdemo.jsp";
     }
 
@@ -105,59 +106,59 @@ public class OrderController {
     	Order[] orders = q.toArray(new Order[]{});
     	return orders[orders.length-1].getAmount();
 
-    }    	
-    
+    }
+
     @RequestMapping(value="/startStream")
     public @ResponseBody String startStream(){
 		logger.warn("Rabbit URI "+client.getRabbitURI());
 		if (client.getRabbitURI()==null) return "Please bind a RabbitMQ service";
-    	
+
     	if (generatingData) return "Data already being generated";
-    	
+
     	generatingData = true;
-    	
+
     	generator.startGen();
     	return "Started";
 
-    }    	
+    }
 
     @RequestMapping(value="/stopStream")
     public @ResponseBody String stopStream(){
 		logger.warn("Rabbit URI "+client.getRabbitURI());
 		if (client.getRabbitURI()==null) return "Please bind a RabbitMQ service";
-    	
+
     	if (!generatingData) return "Not Streaming";
     	generatingData = false;
     	generator.stopGen();
-    	
+
     	return "Stopped";
 
-    }    	
-    
+    }
+
     @RequestMapping(value="/killApp")
     public @ResponseBody String kill(){
 		logger.warn("Killing application instance");
-		System.exit(-1);    	
+		System.exit(-1);
     	return "Killed";
 
-    }       
-    
+    }
+
     @RequestMapping(value="/getHeatMap")
     public @ResponseBody HeatMap getHistograms(){
     	HeatMap heatMap = new HeatMap();
     	for (int i=0; i<HeatMap.states.length; i++){
     		heatMap.addOrderSum(HeatMap.states[i], getOrderSum(HeatMap.states[i]));
-    	}    	
+    	}
 
     	heatMap.assignColors();
     	return heatMap;
 
     }
-    
-    
+
+
     @PreDestroy
     public void shutdownThread(){
-    	
+
     	generator.shutdown();
     }
 
